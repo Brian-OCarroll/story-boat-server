c2170a58f9da256d3fdb7f5d8c0acde35dfebb87
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-06 16:35:48 -0800

    refactored room setup using es6 classes

--- server.js
+++ server.js
@@ -1,89 +1,143 @@
 'use strict';
-require('dotenv').config();
+
 const express = require('express');
-// const mongoose = require('mongoose');
-const morgan = require('morgan');
-// const passport = require('passport');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const morgan = require('morgan');
+const passport = require('passport');
+const socket = require('socket.io');
 
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+
+// const localStrategy = require('./passport/localStrategy');
+// const jwtStrategy = require('./passport/jwtStrategy');
+
+let users = {}
+let rooms = {}
+// default namespace is /
+class lobby {
+  //dont know if need to pass in socket server object
+  constructor(io, name, host) {
+    this.host = host;
+    this.name = name;
+    this.timer = 20;
+    this.room = socket.join(name);
+
+  }
+  createRoom() {
+    //where host creates a socket.io room with this.name
+    //add host to object or something
+  }
+  joinRoom() {
+
+  }
+  startSession() {
+    //once all users are present. Initiate game
+    // by having the host click start
+  }
+  initiateTimer() {
+    // once user starts typing, setInterval timer 
+    //broadcast to every client in room using io.sockets.in('room').emit
+    //if timer expires while still typing and user hasn't submitted
+    //autoSubmit by calling onSubmit, then clear interval
+  }
+  userTyping(){
+    //when user starts typing, 
+  }
+  onSubmit() {
+    // when a user submits their sentence, cycle to the next user for their turn
+    // and clear/ reset the timer until they start typing
+  }
+  endRoom() {
+    //do end room stuff
+  }
+}
+class Room extends Lobby {
+  constructor(name, io) {
+    super()
+    this.host = host;
+    this.name = name;
+    this.timer = 20;
+  }
+}
 const app = express();
-// const server = require('http').createServer(app)
+// const jwtAuth = passport.authenticate('jwt', { session: false });
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
 
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+    credentials: true
+  })
+);
 
-const { PORT, DATABASE_URL } = require('./config');
-let users = [];
-let connections = [];
-
-// server.listen(PORT)
-
-//allow cors
-app.use(cors());
-
-//parse request body
 app.use(express.json());
 
+// passport.use(localStrategy);
+// passport.use(jwtStrategy);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
 
+
+// Custom 404 Not Found Error Handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    res.status(500).json({ message: `Server Error: ${err}` });
+  }
+});
 
 let server;
 
-function runServer(databaseUrl, port = PORT) {
+function runServer(port = PORT) {
+  server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+  // console.log('server:', server);
+  let io = socket(server);
+  //getting info we recieved from client (author and mesage) and sending it to everyone else
+  io.on('connection', (socket) => {
+    console.log('user connected:', socket.id);
+    socket.emit('hello, there are users here');
+    socket.on('CREATE_ROOM', ()=> {
 
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
+    });
+    socket.on('TYPING', function(msg){
+      io.sockets.emit('typeUpdate', msg )
+    });
+    socket.on('subscribe', chat => {
+      socket.join(chat);
+    });
+    socket.on('CHAT', function (data) {
+      io.sockets.in(data.room).emit('CHAT', data);
     });
   });
 }
+    io.on('connection', socket => {
+      console.log('user connected:', socket.id);
 
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  });
-}
+    })
 
 if (require.main === module) {
-  runServer(DATABASE_URL).catch(err => console.error(err));
+  dbConnect();
+  runServer();
 }
-const io = require('socket.io').listen(server)
-console.log(server)
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
-io.on('connection', function(socket) {
-  connections.push(socket);
-  console.log('Connected: %s sockets conected', connections.length)
 
-  //disconnect
-  connections.splice(connections.indexOf(socket), 1);
-  console.log('Disconnected: %s sockets connected', connections.length)
-})
-app.use('*', (req, res) => {
-  return res.status(404).json({ message: 'Not Found' });
-});
-module.exports = { app, runServer, closeServer };
-
-// module.exports = { app };
-
+module.exports = app;
