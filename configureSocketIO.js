bdc2d2799c9f907681970cd2030702ac8a67fa57
Brian O'Carroll <bocarroll36@gmail.com>
2020-01-15 17:10:03 -0800

    added ejs page for testing, and created class socket instantiater

--- configureSocketIO.js
+++ configureSocketIO.js
@@ -1,14 +1,243 @@
-//socket-singletion.js
+module.exports = class Main {
+    //dont know if need to pass in socket server object
+    constructor(io) {
+        // this.host = 'host';
+        this.totalGameCount = 0,
+        this.gameList = []
+        this.loopLimit = 0;
+        this.io = io;
+        this.initiateIO();
+        this.numUsers = 0;
+    }
 
-var socket = require('socket.io');
+    initiateIO() {
+        this.io.on('connection', function (socket) {
+            let addedUser = false;
 
-var SocketSingleton = (function() {
-  this.io = null;
-  this.configure = function(server) {
-    this.io = socket(server);
-  }
+            // when the client emits 'new message', this listens and executes
+            socket.on('new message', function (data) {
+                // we tell the client to execute 'new message'
+                socket.broadcast.emit('new message', {
+                    username: socket.username,
+                    message: data
+                });
+            });
 
-  return this;
-})();
+            // when the client emits 'add user', this listens and executes
+            socket.on('add user', function (username) {
+                if (addedUser) return;
 
-module.exports = SocketSingleton;
+                // we store the username in the socket session for this client
+                socket.username = username;
+                ++this.numUsers;
+                addedUser = true;
+                socket.emit('login', {
+                    numUsers: this.numUsers
+                });
+                // echo globally (all clients) that a person has connected
+                socket.broadcast.emit('user joined', {
+                    username: socket.username,
+                    numUsers: this.numUsers
+                });
+            });
+
+            // when the client emits 'typing', we broadcast it to others
+            socket.on('typing', function () {
+                socket.broadcast.emit('typing', {
+                    username: socket.username
+                });
+            });
+
+            // when the client emits 'stop typing', we broadcast it to others
+            socket.on('stop typing', function () {
+                socket.broadcast.emit('stop typing', {
+                    username: socket.username
+                });
+            });
+
+            // when the user disconnects.. perform this
+            socket.on('disconnect', function () {
+                if (addedUser) {
+                    --this.numUsers;
+                    killGame(socket);
+
+                    // echo globally that this client has left
+                    socket.broadcast.emit('user left', {
+                        username: socket.username,
+                        numUsers: numUsers
+                    });
+                }
+            });
+
+
+            socket.on('joinGame', function () {
+                console.log(socket.username + " wants to join a game");
+
+                let alreadyInGame = false;
+
+                for (let i = 0; i < this.totalGameCount; i++) {
+                    let plyr1Tmp = this.gameList[i]['gameObject']['playerOne'];
+                    let plyr2Tmp = this.gameList[i]['gameObject']['playerTwo'];
+                    if (plyr1Tmp == socket.username || plyr2Tmp == socket.username) {
+                        alreadyInGame = true;
+                        console.log(socket.username + " already has a Game!");
+
+                        socket.emit('alreadyJoined', {
+                            gameId: this.gameList[i]['gameObject']['id']
+                        });
+
+                    }
+
+                }
+                if (alreadyInGame == false) {
+
+
+                    this.gameSeeker(socket);
+
+                }
+
+            });
+
+
+            socket.on('leaveGame', function () {
+
+
+                if (this.totalGameCount == 0) {
+                    socket.emit('notInGame');
+
+                }
+
+                else {
+                    killGame(socket);
+                }
+
+            });
+        });
+    }
+
+    buildGame(socket) {
+
+
+        let gameObject = {};
+        gameObject.id = (Math.random() + 1).toString(36).slice(2, 18);
+        gameObject.playerOne = socket.username;
+        gameObject.playerTwo = null;
+        gameObject.playerThree = null;
+        gameObject.playerFour = null;
+        gameObject.playerFive = null;
+        this.totalGameCount++;
+        this.gameList.push({ gameObject });
+
+        console.log("Game Created by " + socket.username + " w/ " + gameObject.id);
+        io.emit('gameCreated', {
+            username: socket.username,
+            gameId: gameObject.id
+        });
+
+
+    }
+    killGame(socket) {
+
+        let notInGame = true;
+        for (let i = 0; i < this.totalGameCount; i++) {
+
+            let gameId = this.gameList[i]['gameObject']['id']
+            let plyr1Tmp = this.gameList[i]['gameObject']['playerOne'];
+            let plyr2Tmp = this.gameList[i]['gameObject']['playerTwo'];
+            let plyr3Tmp = this.gameList[i]['gameObject']['playerThree'];
+            let plyr4Tmp = this.gameList[i]['gameObject']['playerFour'];
+            let plyr5Tmp = this.gameList[i]['gameObject']['playerFive'];
+            if (plyr1Tmp == socket.username) {
+                --this.totalGameCount;
+                console.log("Destroy Game " + gameId + "!");
+                this.gameList.splice(i, 1);
+                console.log(this.gameList);
+                socket.emit('leftGame', { gameId: gameId });
+                io.emit('gameDestroyed', { gameId: gameId, gameOwner: socket.username });
+                notInGame = false;
+            }
+            else if (plyr2Tmp == socket.username) {
+                this.gameList[i]['gameObject']['playerTwo'] = null;
+                console.log(socket.username + " has left " + gameId);
+                socket.emit('leftGame', { gameId: gameId });
+                console.log(this.gameList[i]['gameObject']);
+                notInGame = false;
+            }
+            else if (plyr3Tmp == socket.username) {
+                this.gameList[i]['gameObject']['playerThree'] = null;
+                console.log(socket.username + " has left " + gameId);
+                socket.emit('leftGame', { gameId: gameId });
+                console.log(this.gameList[i]['gameObject']);
+                notInGame = false;
+            }
+            else if (plyr4Tmp == socket.username) {
+                this.gameList[i]['gameObject']['playerFour'] = null;
+                console.log(socket.username + " has left " + gameId);
+                socket.emit('leftGame', { gameId: gameId });
+                console.log(this.gameList[i]['gameObject']);
+                notInGame = false;
+            }
+            else if (plyr5Tmp == socket.username) {
+                this.gameList[i]['gameObject']['playerFive'] = null;
+                console.log(socket.username + " has left " + gameId);
+                socket.emit('leftGame', { gameId: gameId });
+                console.log(this.gameList[i]['gameObject']);
+                notInGame = false;
+            }
+
+        }
+
+        if (notInGame == true) {
+            socket.emit('notInGame');
+        }
+
+    }
+
+    gameSeeker(socket) {
+        ++this.loopLimit;
+        if ((this.totalGameCount == 0) || (this.loopLimit >= 20)) {
+
+            this.buildGame(socket);
+            this.loopLimit = 0;
+
+        } else {
+            let rndPick = Math.floor(Math.random() * this.totalGameCount);
+            if (this.gameList[rndPick]['gameObject']['playerTwo'] == null) {
+                this.gameList[rndPick]['gameObject']['playerTwo'] = socket.username;
+                socket.emit('joinSuccess', {
+                    gameId: this.gameList[rndPick]['gameObject']['id']
+                });
+
+                console.log(socket.username + " has been added to: " + this.gameList[rndPick]['gameObject']['id']);
+
+            } else if (this.gameList[rndPick]['gameObject']['playerThree'] == null){
+                this.gameList[rndPick]['gameObject']['playerThree'] = socket.username;
+                socket.emit('joinSuccess', {
+                    gameId: this.gameList[rndPick]['gameObject']['id']
+                });
+
+                console.log(socket.username + " has been added to: " + this.gameList[rndPick]['gameObject']['id']);
+            } else if (this.gameList[rndPick]['gameObject']['playerFour'] == null){
+                this.gameList[rndPick]['gameObject']['playerFour'] = socket.username;
+                socket.emit('joinSuccess', {
+                    gameId: this.gameList[rndPick]['gameObject']['id']
+                });
+
+                console.log(socket.username + " has been added to: " + this.gameList[rndPick]['gameObject']['id']);
+            } else if (this.gameList[rndPick]['gameObject']['playerFive'] == null){
+                this.gameList[rndPick]['gameObject']['playerFive'] = socket.username;
+                socket.emit('joinSuccess', {
+                    gameId: this.gameList[rndPick]['gameObject']['id']
+                });
+
+                console.log(socket.username + " has been added to: " + this.gameList[rndPick]['gameObject']['id']);
+            }
+            else {
+                //recursive run if all slots are filled
+                this.gameSeeker(socket);
+            }
+        }
+    }
+
+}
+
--- server.js
+++ server.js
@@ -1,103 +1,29 @@
 'use strict';
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 const socket = require('socket.io');
-let socketSingleton = require('./configureSocketIO');
+const Main = require('./configureSocketIO');  
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
+const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require('./config');
+// const { dbConnect } = require('./db-mongoose');
 
 // const localStrategy = require('./passport/localStrategy');
 // const jwtStrategy = require('./passport/jwtStrategy');
 
-let users = {}
-let rooms = {}
-// default namespace is /
-class Main {
-  //dont know if need to pass in socket server object
-  constructor(io) {
-    // this.host = 'host';
-    this.name = 'main';
-    this.users = {};
-    this.rooms = []
-    this.timer = 20;
-    this.io = io;
-    this.initiateIO();
-  }
-  initiateIO() {
-    this.io.on('connection', (socket) => {
-      console.log('user connected', socket.id)
-      this.users[socket.id] = ''
-      console.log(this.users)
-      // socket.emit('join-main', 'Hello, there are' + this.users.length + 'users and '+ this.rooms.length + 'rooms in the session.')
-      socket.on('createRoom', (roomName, host) => {
-       this.createRoom(roomName, host)
-       io.emit('A new game was created: ', roomName)
-       this.rooms.push(newRoom)
-      })
-      socket.on('registerUsername', (name)=> {
-        if(this.users[socket.id] === name){
-          socket.emit('err', 'name already exists')
-        }else{
-          this.setUsername(socket.id, name)
-        }
-      })
-      socket.on('disconnect', () => {
-        console.log('user disconnected', socket.id)
-        delete this.users[socket.id]
-      })
-    })
-  }
-  setUsername(id, name){
-    this.users[id] = name
-    console.log(this.users)
-  }
-  createRoom(roomName, host) {
-    const newRoom = new Room(roomName, host, this.io);
-    //where host creates a socket.io room with this.name
-    //add host to object or something
-    
-  }
-  joinRoom() {
-    
-  }
-  startSession() {
-    //once all users are present. Initiate game
-    // by having the host click start
-  }
-  initiateTimer() {
-    // once user starts typing, setInterval timer 
-    //broadcast to every client in room using io.sockets.in('room').emit
-    //if timer expires while still typing and user hasn't submitted
-    //autoSubmit by calling onSubmit, then clear interval
-  }
-  userTyping(){
-    //when user starts typing, 
-  }
-  onSubmit() {
-    // when a user submits their sentence, cycle to the next user for their turn
-    // and clear/ reset the timer until they start typing
-  }
-  endRoom() {
-    //do end room stuff
-  }
-}
 
+var indexRouter = require('./routes/index');
 
-class Room {
-  constructor(name, host, io) {
-    this.host = host;
-    this.name = name;
-    this.io = io
-    this.timer = 20;
-  }
-
-}
 const app = express();
 // const jwtAuth = passport.authenticate('jwt', { session: false });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -110,13 +36,17 @@ app.use(
     credentials: true
   })
 );
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // passport.use(localStrategy);
 // passport.use(jwtStrategy);
 
 
+app.use('/', indexRouter);
 
 // Custom 404 Not Found Error Handler
 app.use((req, res, next) => {
@@ -135,55 +65,22 @@ app.use((err, req, res, next) => {
   }
 });
 
-let server;
-
-function runServer(port = PORT) {
-  server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
-  // console.log('server:', server);
-  let io = socket(server);
-  //getting info we recieved from client (author and mesage) and sending it to everyone else
-  // io.on('connection', (socket) => {
-  //   console.log('user connected:', socket.id);
-  //   socket.emit('hello, there are users here');
-  //   socket.on('CREATE_ROOM', ()=> {
-
-  //   });
-  //   socket.on('TYPING', function(msg){
-  //     io.sockets.emit('typeUpdate', msg )
-  //   });
-  //   socket.on('subscribe', chat => {
-  //     socket.join(chat);
-  //   });
-  //   socket.on('CHAT', function (data) {
-  //     io.sockets.in(data.room).emit('CHAT', data);
-  //   });
-  // });
-  const main = new Main(io)
-  // main.createRoom('jews', 'jeff')
-}
 
 // Referenced by both runServer and closeServer. closeServer
 // assumes runServer has run and set `server` to a server object
 let server;
-
 function runServer() {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
+    mongoose.connect(DATABASE_URL, { useNewUrlParser: true}, err => {
       if (err) {
         return reject(err);
       }
       server = app
         .listen(PORT, () => {
           console.log(`Your app is listening on port ${PORT}`);
-          // let io = socket(server);
-          socketSingleton.configure(server);
+          let io = socket(server, {forceNew: true});
+          new Main(io)
+          // app.set('socketio', io);
           resolve();
         })
         .on('error', err => {
@@ -209,8 +106,8 @@ function closeServer() {
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+  runServer().catch(err => console.error(err));
+
 }
 
 module.exports = app;
